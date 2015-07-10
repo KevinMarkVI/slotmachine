eeda816df7a5a9fec301eb9eacccc41f160c9da8
Kevin Berg <kevinmarkvi@gmail.com>
2015-07-10 17:45:10 -0600

    Add images

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 $(function(){
 
-//TODO
+  var changeImage = function() {
+    console.log("CALLED");
+    var img1 = "./images/teapot.jpg";
+    var img2 = "./images/coffeemaker.jpg";
+    var img3 = "./images/espressomachine.jpg";
+    var current = $('#slot1').attr('src');
 
+    if (current === img1) {
+      console.log('1');
+      $('#slot1').attr('src', img2);
+    } 
+    if (current === img2) {
+      console.log('2');
+      $('#slot1').attr('src', img3);
+    } 
+    if (current === img3) {
+      console.log('3');
+      $('#slot1').attr('src', img1);
+    }
+  };
+  setInterval(changeImage, 100);
 });
+
+
+
+
