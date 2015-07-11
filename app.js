82c8ef8f68093f09cb90d2bb51eb5cbb8d7bacf4
Kevin Berg <kevinmarkvi@gmail.com>
2015-07-10 23:14:08 -0600

    Add features spin and stop

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 $(function(){
 
-  var changeImage = function() {
-    console.log("CALLED");
-    var img1 = "./images/teapot.jpg";
-    var img2 = "./images/coffeemaker.jpg";
-    var img3 = "./images/espressomachine.jpg";
-    var current = $('#slot1').attr('src');
+  var stopSpin = function() {
+    clearInterval(slot1);
+    setTimeout(function() {clearInterval(slot2);}, 400);
+    setTimeout(function() {clearInterval(slot3);}, 1000);
+  };
 
+  var changeImage = function(id, img1, img2, img3) {
+    var current = $(id).attr('src');
     if (current === img1) {
-      console.log('1');
-      $('#slot1').attr('src', img2);
+      $(id).attr('src', img2);
     } 
     if (current === img2) {
-      console.log('2');
-      $('#slot1').attr('src', img3);
+      $(id).attr('src', img3);
     } 
     if (current === img3) {
-      console.log('3');
-      $('#slot1').attr('src', img1);
+      $(id).attr('src', img1);
     }
   };
-  setInterval(changeImage, 100);
+
+  var startSpin = function() {
+    slot1 = setInterval(function() {changeImage('#slot1', './images/coffeemaker.jpg', './images/teapot.jpg', './images/espressomachine.jpg');}, 100);
+    slot2 = setInterval(function() {changeImage('#slot2', './images/teastrainer.jpg', './images/coffeefilter.jpg', './images/espressotamper.jpg');}, 100);
+    slot3 = setInterval(function() {changeImage('#slot3', './images/coffeegrounds.jpg', './images/looseleaftea.jpg', './images/groundespresso.jpg');}, 100);
+    setTimeout(stopSpin, 2000);
+  };
+
+  $('button').click(function() {
+    startSpin();
+  });
 });
+
+
+
 
 
 
