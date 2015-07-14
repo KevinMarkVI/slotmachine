2e8a1c39545a52318af449b099f2c5908a3e47de
Kevin Berg <kevinmarkvi@gmail.com>
2015-07-13 21:21:42 -0700

    "Add version 2"

--- slotmachine.js
+++ slotmachine.js
@@ -1,12 +1,29 @@
-var SlotMachine = function() {
-  this.slot1 = [1, 2, 3];
-  this.slot2 = [1, 2, 3];
-  this.slot3 = [1, 2, 3];
+var SlotMachine = function(el) {
+  this.slot1 = [1,2,3];
+  this.slot2 = [4,5,6];
+  this.slot3 = [7,8,9];
+  this.el = el;
 };
 
 
-SlotMachine.prototype.spin = function()  { 
-  return '' + this.slot1[slotIndex()] + '' + this.slot2[slotIndex()] + '' +  this.slot3[slotIndex()] + '';
+
+SlotMachine.prototype.play = function() {
+  var spinMachine = spin(this.slot1, this.slot2, this.slot3);
+  var selections = spinMachine.join();
+  if (selections === '1,4,7') {
+    console.log("YOU WIN COFFEE!!!");
+  } else if (selections === '2,5,8') {
+    console.log("YOU WIN TEA!!!");
+  } else if (selections === '3,6,9') {
+    console.log("YOU WIN ESPRESSO!!!");
+  } else {
+    console.log("YOU LOSE!!! TRY AGAIN!");
+  }
+};
+
+var spin = function(arr1, arr2, arr3)  { 
+  var results = [arr1[slotIndex()], arr2[slotIndex()], arr3[slotIndex()]];
+  return results;
 };
 
 var slotIndex = function() {
@@ -20,10 +37,35 @@ var slotIndex = function() {
   }
 };
 
-var result = function(str) {
-  if (str === '111' || str === '222' || str === '333') {
-    console.log("YOU WIN!!!!");
-  } else {
-    console.log("YOU LOSE!!! TRY AGAIN!");
+var slots = {
+  '1': './images/coffeemaker.jpg',
+  '2': './images/teapot.jpg',
+  '3': './images/espressomachine.jpg',
+  '4': './images/coffeefilter.jpg',
+  '5': './images/teastrainer.jpg',
+  '6': './images/espressotamper.jpg',
+  '7': './images/coffeegrounds.jpg',
+  '8': './images/looseleaftea.jpg',
+  '9': './images/groundespresso.jpg'
+};
+
+$(function() {
+  $('button').click(function() {
+    console.log("BUTTON WORKS");
+  });
+  loadImages('#machine');
+});
+
+
+var loadImages = function(target, pathArr) {
+  pathArr = pathArr || [slots['1'], slots['5'], slots['9']];
+  for (var i = 0; i < pathArr.length; i++){
+    $('<img id="slot'+ i +'" src="'+ pathArr[i] +'">').load(function() {
+      $(this).appendTo(target);
+    });
   }
 };
+
+
+
+
