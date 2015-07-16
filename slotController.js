4f2225415c3fe8f130133be11e3bf3605cb74da9
Kevin Berg <kevinmarkvi@gmail.com>
2015-07-15 19:18:41 -0700

    Add tests

--- slotController.js
+++ slotController.js
@@ -3,6 +3,9 @@ $(function() {
   var machine = new SlotMachine();
   machine.loadImages('#machine');
 
+  //Tests
+  runTests();
+
   $('button').click(function() {
     $('h2').remove();
     var play = machine.spin();
@@ -19,12 +22,14 @@ var UIController = function(play) {
   this.play = play;
 };
 
+//Supplies and calls function to vary the images
 UIController.prototype.uiSpin = function() {
   this.slot0 = setInterval((function() {this.spinImage('#slot0', slots['1'], slots['2'], slots['3']);}).bind(this), 100);
   this.slot1 = setInterval((function() {this.spinImage('#slot1', slots['4'], slots['5'], slots['6']);}).bind(this), 100);
   this.slot2 = setInterval((function() {this.spinImage('#slot2', slots['7'], slots['8'], slots['9']);}).bind(this), 100);
 };
 
+//Stops the images and sets the correct images from the randomly created results
 UIController.prototype.stopSpin = function() {
   var results = this.play; //fix
   clearInterval(this.slot0);
@@ -37,10 +42,12 @@ UIController.prototype.stopSpin = function() {
   setTimeout(function() {$('#slot2').attr('src', slots[results[2]]);}, 1300);
 };
 
+//Alerts the user of the results
 UIController.prototype.loadResult = function(notification) {
   $('<h2></h2>').text(winnerNotifications[notification]).appendTo('#resultsDisplay');
 };
 
+//Rotates the images on the DOM
 UIController.prototype.spinImage = function(id, img1, img2, img3) {
   var current = $(id).attr('src');
   if (current === img1) {
--- slotmachine.js
+++ slotmachine.js
@@ -24,6 +24,7 @@ var slots = {
   9: './images/groundespresso.jpg'
 };
 
+
 var SlotMachine = function() {
   this.slot1 = [1,2,3];
   this.slot2 = [4,5,6];
