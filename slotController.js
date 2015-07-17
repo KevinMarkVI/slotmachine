eb353bf6af3216d9e02f0058d1d12f9c64d6891b
Kevin Berg <kevinmarkvi@gmail.com>
2015-07-16 18:37:04 -0700

    Final Version

--- slotController.js
+++ slotController.js
@@ -1,63 +1,71 @@
 
 $(function() {
+  $('.animation').hide();
   var machine = new SlotMachine();
-  machine.loadImages('#machine');
-
+  var clicked = false;
   //Tests
   runTests();
 
   $('button').click(function() {
-    $('h2').remove();
-    var play = machine.spin();
-    var uiMachine = new UIController(play);
-    uiMachine.uiSpin();
-    setTimeout(function () { uiMachine.stopSpin(); }, 2000);
-    var winner = machine.isWinner(play);
-    setTimeout(function() {uiMachine.loadResult(winner);}, 3400);
+    if (!clicked) {
+      clicked = true;
+      var play = machine.spin();
+      var uiMachine = new UIController(play);
+      uiMachine.buttonClick(machine);
+      setTimeout(function() {clicked = false;}, slotTimings.animationSpin + slotTimings.slotStopInterval * 3);
+    }
   });
 });
 
-//KB: Context global issue.
+var slotTimings = {
+  'slotStopInterval': 500,
+  'animationSpin': 2000
+};
+
+var resultNotifications = {
+  0: "You win a hot cup of coffee!!!",
+  1: "You win a hot cup of tea!!!",
+  2: "You win a hot cup of espresso!!!",
+  3: "Not a winner!!! Try again!!!"
+};
+
+var slotImages = {
+  1: ['./images/coffeemaker.jpg', './images/teapot.jpg', './images/espressomachine.jpg'],
+  2: ['./images/coffeefilter.jpg', './images/teastrainer.jpg', './images/espressotamper.jpg'],
+  3: ['./images/coffeegrounds.jpg', './images/looseleaftea.jpg', './images/groundespresso.jpg']
+};
+
 var UIController = function(play) {
   this.play = play;
 };
 
-//Supplies and calls function to vary the images
-UIController.prototype.uiSpin = function() {
-  this.slot0 = setInterval((function() {this.spinImage('#slot0', slots['1'], slots['2'], slots['3']);}).bind(this), 100);
-  this.slot1 = setInterval((function() {this.spinImage('#slot1', slots['4'], slots['5'], slots['6']);}).bind(this), 100);
-  this.slot2 = setInterval((function() {this.spinImage('#slot2', slots['7'], slots['8'], slots['9']);}).bind(this), 100);
-};
-
 //Stops the images and sets the correct images from the randomly created results
 UIController.prototype.stopSpin = function() {
-  var results = this.play; //fix
-  clearInterval(this.slot0);
-  $('#slot0').attr('src', slots[results[0]]);
-
-  setTimeout((function() {clearInterval(this.slot1);}).bind(this), 700);
-  setTimeout(function() {$('#slot1').attr('src', slots[results[1]]);}, 800);
-
-  setTimeout((function() {clearInterval(this.slot2);}).bind(this), 1200);
-  setTimeout(function() {$('#slot2').attr('src', slots[results[2]]);}, 1300);
+  var results = this.play; 
+  $('#animFrame1').hide();
+  $('.staticImage1').attr('src', slotImages[1][results[0]]).show();
+  setTimeout(function() {$('#animFrame2').hide(); $('.staticImage2').attr('src', slotImages[2][results[1]]).show();}, slotTimings.slotStopInterval);
+  setTimeout(function() {$('#animFrame3').hide(); $('.staticImage3').attr('src', slotImages[3][results[2]]).show();}, slotTimings.slotStopInterval * 2);
 };
 
 //Alerts the user of the results
-UIController.prototype.loadResult = function(notification) {
-  $('<h2></h2>').text(winnerNotifications[notification]).appendTo('#resultsDisplay');
-};
-
-//Rotates the images on the DOM
-UIController.prototype.spinImage = function(id, img1, img2, img3) {
-  var current = $(id).attr('src');
-  if (current === img1) {
-    $(id).attr('src', img2);
-  } 
-  if (current === img2) {
-    $(id).attr('src', img3);
-  } 
-  if (current === img3) {
-    $(id).attr('src', img1);
+UIController.prototype.loadResult = function(winner) {
+  var notification;
+  if (winner) {
+    notification = this.play[0];
+  } else {
+    notification = 3;
   }
+  $('<h2></h2>').text(resultNotifications[notification]).appendTo('#resultsDisplay');
 };
 
+UIController.prototype.buttonClick = function(machine) {
+    $('.staticImage1').hide();
+    $('.staticImage2').hide();
+    $('.staticImage3').hide();
+    $('.animation').show();
+    $('h2').remove();
+    var winner = machine.isWinner(this.play);  
+    setTimeout((function () {this.stopSpin(); }).bind(this), slotTimings.animationSpin);
+    setTimeout((function() {this.loadResult(winner);}).bind(this), slotTimings.animationSpin + slotTimings.slotStopInterval * 2);
+};
--- slotmachine.js
+++ slotmachine.js
@@ -1,57 +1,23 @@
-var slotMachineType = {
-  'coffee': 1,
-  'tea': 2,
-  'espresso': 3,
-  'noWin': 4
-};
-
-var winnerNotifications = {
-  1: "YOU WIN A HOT CUP OF COFFEE!!!",
-  2: "YOU WIN A HOT CUP OF TEA!!!",
-  3: "YOU WIN A HOT CUP OF ESPRESSO!!!",
-  4: "NOT A WINNER!!! TRY AGAIN!!!"
-};
-
-var slots = {
-  1: './images/coffeemaker.jpg',
-  2: './images/teapot.jpg',
-  3: './images/espressomachine.jpg',
-  4: './images/coffeefilter.jpg',
-  5: './images/teastrainer.jpg',
-  6: './images/espressotamper.jpg',
-  7: './images/coffeegrounds.jpg',
-  8: './images/looseleaftea.jpg',
-  9: './images/groundespresso.jpg'
-};
-
 
 var SlotMachine = function() {
-  this.slot1 = [1,2,3];
-  this.slot2 = [4,5,6];
-  this.slot3 = [7,8,9];
 };
 
 //Creates a random slot machine pull
 SlotMachine.prototype.spin = function() {
-  var result = [this.slot1[this.randomSlotIndex()], this.slot2[this.randomSlotIndex()], this.slot3[this.randomSlotIndex()]];
+  var result = [this.randomSlot(), this.randomSlot(), this.randomSlot()];
   return result;
 };
 
-//Detirmines whether the random pull is a winner and if so returns the appropriate response using the winner notifications object
+//Determines whether the random pull is a winner and if so returns the appropriate response using the winner notifications object
 SlotMachine.prototype.isWinner = function(arr) {
-  if (arr[0] === 1 && arr[1] === 4 && arr[2] === 7) {
-    return slotMachineType.coffee;
-  } else if (arr[0] === 2 && arr[1] === 5 && arr[2] === 8) {
-    return slotMachineType.tea;
-  } else if (arr[0] === 3 && arr[1] === 6 && arr[2] === 9) {
-    return slotMachineType.espresso;
-  } else {
-    return slotMachineType.noWin;
+  if (arr[0] === arr[1] && arr[1] === arr[2]) {
+    return true;
   }
+  return false;
 };
 
 //Creates random indexes to make a selection from slot1, slot2, and slot3
-SlotMachine.prototype.randomSlotIndex = function() {
+SlotMachine.prototype.randomSlot = function() {
   var rand = Math.random(); 
   if (rand <= .33) {
     return 0;
@@ -61,21 +27,6 @@ SlotMachine.prototype.randomSlotIndex = function() {
     return 2;
   }
 };
-
-//Loads the initial images to the page
-//TODO: Get more images that explain what each frame uses.
-SlotMachine.prototype.loadImages = function(target) {
-  pathArr = [slots['1'], slots['5'], slots['9']];
-  for (var i = 0; i < pathArr.length; i++){
-    var img = $('<img>').attr('id', 'slot'+ i).attr('src', pathArr[i]);
-    img.load(function() {
-      $(this).appendTo(target);
-    });
-  }
-};
-
-
-
 
 
 
